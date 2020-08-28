68ab0d87b365596f4d8943b3daffbd222d2e436c
Armando <rivera.armando997@gmail.com>
2020-08-27 23:56:21 -0500

    Card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  public data = {
+    img: 'https://rtvc-assets-radionica3.s3.amazonaws.com/s3fs-public/styles/image_750x424/public/field/image/article/rick-morty-pelicula.jpg?itok=thJFYqHN',
+    name: 'Pilot',
+    airDate: 'Today',
+    episode: 'S0E1'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
