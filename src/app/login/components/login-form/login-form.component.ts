ee0fa5dabbb98da899fea14ee472f4229f44e6dc
Armando <rivera.armando997@gmail.com>
2020-08-28 13:50:52 -0500

    Login form validations and styles

--- src/app/login/components/login-form/login-form.component.ts
+++ src/app/login/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  invalidForm(form, field: string): boolean {
+    return form.get(field).invalid && form.get(field).touched;
+  }
+
+  createForm(): void {
+    this.loginForm = this.fb.group({
+      // Define properties
+      email: ['', [Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required]],
+      password: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(8)]]
+    });
+  }
+
+  send(): void {
+
+    const user = this.loginForm.value;
+
+    if (this.loginForm.invalid) {
+      Object.values(this.loginForm.controls).map(control => {
+        control.status === 'INVALID' && control.markAsTouched();
+      });
+      return;
+    }
+
+
+    // Post information
+
+    // Reset form
+    this.loginForm.reset();
+
+  }
 }
