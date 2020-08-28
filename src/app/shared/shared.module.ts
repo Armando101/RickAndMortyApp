393a69d90f6cd0cf09dd8b06ef4c3c9da804d2ad
Armando <rivera.armando997@gmail.com>
2020-08-27 22:35:40 -0500

    Base Structure, shared and home module, layout and preloading strategy

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, CardComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    CardComponent
+  ]
+})
+export class SharedModule { }
